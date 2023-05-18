0e24fdc47c66a9033647a1f9b538d30309f4c711
Mitchell <mgantz22@icloud.com>
2023-05-18 11:12:50 -0500

    Create Health bar and slider

---
+++ Assets/Scripts/PlayerUI&Health/HealthBar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthbarSlider;
+
+    public void GiveFUllHealth(float health)
+    {
+        healthbarSlider.maxValue = health;
+        healthbarSlider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        healthbarSlider.value = health;
+    }
+}
