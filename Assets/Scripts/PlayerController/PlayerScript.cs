a0369d32b965cd104f88d6cb4d472d4aa872c98b
Mitchell <mgantz22@icloud.com>
2023-05-18 11:12:00 -0500

    Update to add player health and energy

--- Assets/Scripts/PlayerController/PlayerScript.cs
+++ Assets/Scripts/PlayerController/PlayerScript.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Header("Player Health & Energy")]
+    private float playerHealth = 200f;
+    public float presentHealth;
+    public HealthBar healthbar;
+    private float playerEnergy = 100f;
+    public float presentEnergy;
+    public EnergyBar energyBar;
+    public GameObject DamageIndicator;
+
+
     [Header("Player Movement")]
     public float movementSpeed = 5f;
     public float rotSpeed = 450f;
@@ -12,10 +22,8 @@ public class PlayerScript : MonoBehaviour
     Quaternion requiredRotation;
     bool playerControl = true;
 
-
     [Header("Player Animator")]
     public Animator animator;
-
 
     [Header("Player Collision & Gravity")]
     public CharacterController CC;
@@ -30,10 +38,41 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] Vector3 requiredMoveDir;
     Vector3 velocity;
 
-
+    private void Awake()
+    {
+        presentHealth = playerHealth;
+        presentEnergy = playerEnergy;
+        healthbar.GiveFUllHealth(presentHealth);
+        energyBar.GiveFUllEnergy(presentEnergy);
+    }
 
     private void Update()
     {
+        if(presentEnergy <= 0)
+        {
+            movementSpeed = 2f;
+
+            if(!Input.GetButton("Horizontal") || !Input.GetButton("Vertical"))
+            {
+                animator.SetFloat("movementValue", 0f);
+            }
+
+            if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+            {
+                animator.SetFloat("movementValue", 0.5f);
+                StartCoroutine(setEnergy());
+            }
+        }
+
+        if(presentEnergy >= 1)
+        {
+            movementSpeed = 5f;
+        }
+
+        if (animator.GetFloat("movementValue") >= 0.9999)
+        {
+            playerEnergyDecrease(0.02f);
+        }
         if(!playerControl)
             return;
 
@@ -134,5 +173,44 @@ public class PlayerScript : MonoBehaviour
     {
         get => playerControl;
         set => playerControl = value;
+    }
+
+    public void playerHitDamage(float takeDamage)
+    {
+        presentHealth -= takeDamage;
+        healthbar.SetHealth(presentHealth);
+        StartCoroutine(showDamage());
+
+        if(presentHealth <= 0)
+        {
+            PlayerDie();
+        }
+    }
+
+    private void PlayerDie()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Object.Destroy(gameObject, 1.0f);
+    }
+
+    public void playerEnergyDecrease(float energyDecrease)
+    {
+        presentEnergy -= energyDecrease;
+        energyBar.SetEnergy(presentEnergy);
+    }
+
+    IEnumerator setEnergy()
+    {
+        presentEnergy = 0f;
+        yield return new WaitForSeconds(5f);
+        energyBar.GiveFUllEnergy(presentEnergy);
+        presentEnergy = 100f;
+    }
+
+    IEnumerator showDamage()
+    {
+        DamageIndicator.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        DamageIndicator.SetActive(false);
     }
 }
