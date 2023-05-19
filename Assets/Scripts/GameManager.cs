82a63864ddf4f96eea4214fbb2bc171e76e1d66c
Mitchell <mgantz22@icloud.com>
2023-05-19 08:40:30 -0500

    Create game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int numberofGrenades;
+    public int numberofHealth;
+    public int numberofEnergy;
+
+}
