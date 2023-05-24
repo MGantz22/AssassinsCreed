bcfb289137c97c0d495b1dae5f13611a68821481
Mitchell <mgantz22@icloud.com>
2023-05-23 21:43:55 -0500

    update stock of pots

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,19 @@ public class GameManager : MonoBehaviour
     public int numberofHealth;
     public int numberofEnergy;
 
+    [Header("Stocks")]
+    public Text GrenadeStock1;
+    public Text GrenadeStock2;
+    public Text HealthStock;
+    public Text EnergyStock;
+
+    [Header("Health & Energy")]
+    public GameObject healthSlot;
+    public GameObject energySlot;
+
+
+
+
     [Header("Ammo & Mag")]
     public Rifle rifle;
     public Bazooka bazooka;
@@ -19,9 +32,36 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        //show Ammo and Mag stock for Guns
         RifleAmmoText.text = "" + rifle.presentAmmunition;
         RifleMagText.text = "" + rifle.mag;
         BazookaAmmoText.text = "" + bazooka.presentAmmunition;
         BazookaMagText.text = "" + bazooka.mag;
+
+        //show stock for grenade and pots
+        GrenadeStock1.text = "" + numberofGrenades;
+        GrenadeStock2.text = "" + numberofGrenades;
+        HealthStock.text = "" + numberofHealth;
+        EnergyStock.text = "" + numberofEnergy;
+
+        if(numberofHealth > 0)
+        {
+            healthSlot.SetActive(true);
+        }
+        else if(numberofHealth <= 0)
+        {
+            healthSlot.SetActive(false);
+        }
+
+        if(numberofEnergy > 0)
+        {
+            energySlot.SetActive(true);
+        }
+        else if(numberofEnergy <= 0)
+        {
+            energySlot.SetActive(false);
+        }
+
+
     }
 }
